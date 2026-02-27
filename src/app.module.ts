860f3944642314ef78d73e409105f2b8a4312639
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-27 18:34:24 +0700

    env prod && dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {DbModule} from './db.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [DbModule],
 })
 export class AppModule {}
---
+++ src/db.module.ts
@@ -0,0 +1,19 @@
+import {Module} from '@nestjs/common';
+import {SequelizeModule} from '@nestjs/sequelize';
+import {ConfigModule} from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({envFilePath: `.${process.env.NODE_ENV}.env`}),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadModels: true,
+    }),
+  ],
+})
+export class DbModule {}
