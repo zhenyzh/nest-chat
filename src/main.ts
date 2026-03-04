888e3fa1bdfcd94dfdebce689d827edd17eaa56e
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-04 11:46:14 +0700

    add global preffix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from './app.module';
 async function start() {
   const PORT = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   await app.listen(PORT, () => console.log(`Server start on port = ${PORT} `));
 }
 
