b4abb36de4a036dfdfec9ea37dc80ea4ef22132d
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-06 22:38:25 +0700

    add url client

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+
 async function start() {
   const PORT = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+
+  app.enableCors({
+    origin: String(process.env.CLIENT_URL),
+    credentials: true,
+  });
+
   await app.listen(PORT, () => console.log(`Server start on port = ${PORT} `));
 }
 
