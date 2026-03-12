68b2382336db05fc190a8baa6447cc231e8737bb
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-12 22:12:26 +0700

    fix

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,12 +1,16 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, UseGuards} from '@nestjs/common';
 import {ChatsService} from './chats.service';
+import {JwtAuthGuard} from '../auth/jwt-auth.guard';
+import type {Request} from 'express';
 
 @Controller('chats')
 export class ChatsController {
   constructor(private chatsService: ChatsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('open')
-  openChat(@Body() body: {userIdMe: number; userIdOther: number}) {
-    return this.chatsService.openChat(body.userIdMe, body.userIdOther);
+  openChat(@Body() body: {userIdOther: number}, @Req() req: Request) {
+    const user = (req as any).user;
+    return this.chatsService.openChat(user.id, body.userIdOther);
   }
 }
