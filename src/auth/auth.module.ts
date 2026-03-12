5d5c8081b22c0284350c45588d6aaaec71e15c00
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-12 10:03:56 +0700

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,6 @@ import {forwardRef, Module} from '@nestjs/common';
 import {AuthController} from './auth.controller';
 import {AuthService} from './auth.service';
 import {UsersModule} from '../users/users.module';
-import {JwtModule} from '@nestjs/jwt';
-import {TOKEN} from '../../utils/token';
 import {TokensModule} from '../token/tokens.module';
 
 @Module({
